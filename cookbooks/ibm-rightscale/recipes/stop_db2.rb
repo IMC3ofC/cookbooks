e404fdcb3024015692d0bbfacedc643aa302c6ac
Bradley Steinfeld <bradley@steinfeld.ca>
2013-07-11 14:47:11 -0400

    For testing

--- cookbooks/ibm-rightscale/recipes/stop_db2.rb
+++ cookbooks/ibm-rightscale/recipes/stop_db2.rb
@@ -3,6 +3,7 @@ rightscale_marker :begin
 log "Stopping DB2"
 
 execute_as_user "db2stop" do
+  user node[:db2][:instance][:username]
   command "db2stop #{node[:db2][:force] == 'yes' ? 'force' : ''}"
   action :run
 end
