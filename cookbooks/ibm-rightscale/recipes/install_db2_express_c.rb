6dff95feada38db818f8132d328dfc4b150bc815
Bradley Steinfeld <bradley@steinfeld.ca>
2013-07-11 11:14:09 -0400

    For testing

--- cookbooks/ibm-rightscale/recipes/install_db2_express_c.rb
+++ cookbooks/ibm-rightscale/recipes/install_db2_express_c.rb
@@ -40,14 +40,21 @@ geo           = "us-east-1"
 to_download = IMCloudClient.download_url('DB2 Express-C 10.5', { :cloud => storage_cloud, :geography => geo })
 files       = to_download.first["download"]["url"][5..-1].split("/",2)
 
+puts "Key: " + to_download.first["download"]["key"]
+puts "Secret: " + to_download.first["download"]["secret"]
+
 @ros = RightScale::Tools::ROS.factory("s3", to_download.first["download"]["key"], to_download.first["download"]["secret"])
+
+install_media_location = File.join("/tmp", files.last.split("/").last)
 
 log "Need to download: #{files.first} / #{files.last}"
 log "To: #{File.join("/tmp", files.last.split("/").last)}"
 begin
-  @ros.get_object_to_file(files.first, files.last, File.join("/tmp", files.last.split("/").last))
+  @ros.get_object_to_file(files.first, files.last, install_media_location)
 rescue Exception => msg  
   log msg
+  log msg.backtrace
+  log msg.class
 end
 
 log "Installing DB2 Express-C 10.5"
@@ -118,7 +125,7 @@ else
 	notifies :run, "execute[install-db2]", :immediately
   end
   
-  file "/tmp/v10.5_linuxx64_*.tar.gz" do
+  file install_media_location do
     action :delete
   end
   
