ca4118bd03a8a61b230c1963e16a324b072e6830
Bradley Steinfeld <bradley@steinfeld.ca>
2013-07-11 14:45:38 -0400

    For testing

--- cookbooks/ibm-rightscale/definitions/execute_as_user.rb
+++ cookbooks/ibm-rightscale/definitions/execute_as_user.rb
@@ -2,5 +2,8 @@ define :execute_as_user, :user => "root", :command => nil do
   log params.inspect
   execute params[:name] do
     command "su - #{params[:user]} -c \"#{params[:command]}\""
+	params.delete_if { |key, value| [:user, :command].include?(key) }.each do |meth, value|
+	  send(meth, value)
+	end
   end
 end
