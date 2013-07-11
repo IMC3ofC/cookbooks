f351cde84c4fc0d57b8786c0ab48162e2f8d28ae
Bradley Steinfeld <bradley@steinfeld.ca>
2013-07-11 10:56:44 -0400

    For testing

--- cookbooks/ibm-rightscale/recipes/install_db2_express_c.rb
+++ cookbooks/ibm-rightscale/recipes/install_db2_express_c.rb
@@ -40,10 +40,10 @@ geo           = "us-east-1"
 to_download = IMCloudClient.download_url('DB2 Express-C 10.5', { :cloud => storage_cloud, :geography => geo })
 files       = to_download.first["download"]["url"][5..-1].split("/",2)
 
-log "Key: #{to_download.first["download"]["key"]}"
-log "Secret: #{to_download.first["download"]["secret"]}"
+@ros = RightScale::Tools::ROS.factory("s3", to_download.first["download"]["key"], to_download.first["download"]["secret"])
 
-#@ros = RightScale::Tools::ROS.factory("s3", to_download.first["download"]["key"], to_download.first["download"]["secret"])
+log "Need to download: #{files.first} / #{files.last}"
+log "To: #{File.join("/tmp", files.last.split("/").last)}"
 #@ros.get_object_to_file(files.first, files.last, File.join("/tmp", files.last.split("/").last))
 
 log "Installing DB2 Express-C 10.5"
