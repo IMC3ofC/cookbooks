fd084063fee4dbe1c779a3c1f41f49ddc40b9405
Bradley Steinfeld <bradley@steinfeld.ca>
2013-07-12 07:24:39 -0400

    For testing

--- cookbooks/ibm-rightscale/recipes/backup_database.rb
+++ cookbooks/ibm-rightscale/recipes/backup_database.rb
@@ -5,11 +5,12 @@ log "Backup DB2 Database"
 
 log "Running Database Backup"
 
-execute_as_user "create-database" do
+cd = execute_as_user "create-database" do
   command "db2 backup DB #{node[:db2][:database][:name]} #{node[:db2][:database][:options]}"
   user node[:db2][:instance][:username]
-  action :run
+  action :nothing
 end
+cd.run_action(:run)
 
 if node[:backup][:save_to_cloud] == "yes"
   log "Saving Backup to cloud"
