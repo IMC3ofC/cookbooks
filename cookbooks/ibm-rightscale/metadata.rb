f2bb31415a9a369cd6bf4c1010c896dbe254de81
Bradley Steinfeld <bradley@steinfeld.ca>
2013-09-05 11:28:56 -0400

    Updated metadata to v0.2.0

--- cookbooks/ibm-rightscale/metadata.rb
+++ cookbooks/ibm-rightscale/metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'imcloud@ca.ibm.com'
 license          'All rights reserved'
 description      'Installs/Configures ibm-rightscale'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.1.0'
+version          '0.2.0'
 
 depends "rightscale"
 
